c42adc2d5ca38f11b52dac9b14ff4f6906588e89
Shantanu Singh <shannformal323@gmail.com>
2023-05-31 10:59:59 +0530

    swagger UI added

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
 
+// swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 // rate limit package
 app.set("trust proxy", 1);
 const limiter = rateLimit({
@@ -27,6 +32,11 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get("/", (req, res) => {
+  res.send("<h1>jobs API</h1><a href='/api-docs'>Documentation</a>");
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 const authRouter = require("./routes/auth");
